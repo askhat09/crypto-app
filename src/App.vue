863f43fe55acc41e76811377420f9d959465002f
Askhat <sab@technodom.kz>
2021-05-30 22:32:47 +0600

    rewrite css

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,44 @@
 <template>
-<div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+<div class="app-wrapper container">
   <div class="container">
     <section>
       <div class="flex">
-        <div class="max-w-xs">
-          <label for="wallet" class="block text-sm font-medium text-gray-700"
+        <div class="ticker">
+          <label for="wallet" class="ticker-label"
             >Тикер</label
           >
-          <div class="mt-1 relative rounded-md shadow-md">
+          <div class="ticker-input_wrapper">
             <input
               v-model="ticker"
               type="text"
               name="wallet"
               id="wallet"
-              class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+              class="ticker-input"
               placeholder="Например DOGE"
             />
           </div>
-          <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
-            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+          <div class="flex tags">
+            <span class="tag">
               BTC
             </span>
-            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+            <span class="tag">
               DOGE
             </span>
-            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+            <span class="tag">
               BCH
             </span>
-            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+            <span class="tag">
               CHD
             </span>
           </div>
-          <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
+          <div class="ticker-error-message">Такой тикер уже добавлен</div>
         </div>
       </div>
       <button
         @click="add"
         type="button"
-        class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        class="ticker-add-button"
       >
-        <!-- Heroicon name: solid/mail -->
         <svg
           class="-ml-0.5 mr-2 h-6 w-6"
           xmlns="http://www.w3.org/2000/svg"
@@ -56,25 +55,25 @@
       </button>
     </section>
     <template v-if='tickers.length'>
-      <hr class="w-full border-t border-gray-600 my-4" />
-      <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+      <hr class="tickers-hr" />
+      <dl class="tickers-wrapper">
         <div
           v-for="t of tickers"
           :key='t.name'
-          class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+          class="tickers-item"
         >
-          <div class="px-4 py-5 sm:p-6 text-center">
-            <dt class="text-sm font-medium text-gray-500 truncate">
+          <div class="tickers-item-info">
+            <dt class="tickers-item-info-label">
               {{ t.name }}
             </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">
+            <dd class="tickers-item-info-price">
               {{ t.price }}
             </dd>
           </div>
-          <div class="w-full border-t border-gray-200"></div>
+          <div class="tickers-item-border"></div>
           <button
             @click="deleteTicker(t)"
-            class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+            class="tickers-item-button"
           >
             <svg
               class="h-5 w-5"
@@ -91,28 +90,28 @@
           </button>
         </div>
       </dl>
-      <hr class="w-full border-t border-gray-600 my-4" />
-    <section class="relative">
-      <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+      <hr class="tickers-hr" />
+    <section class="ticker-graphic">
+      <h3 class="ticker-graphic-heading">
         VUE - USD
       </h3>
-      <div class="flex items-end border-gray-600 border-b border-l h-64">
+      <div class="ticker-graphic-info">
         <div
-          class="bg-purple-800 border w-10 h-24"
+          class="ticker-graphic-info-item"
         ></div>
         <div
-          class="bg-purple-800 border w-10 h-32"
+          class="ticker-graphic-info-item"
         ></div>
         <div
-          class="bg-purple-800 border w-10 h-48"
+          class="ticker-graphic-info-item"
         ></div>
         <div
-          class="bg-purple-800 border w-10 h-16"
+          class="ticker-graphic-info-item"
         ></div>
       </div>
       <button
         type="button"
-        class="absolute top-0 right-0"
+        class="ticker-graphic-button"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
